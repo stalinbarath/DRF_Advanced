e68ab2d32bfb40ce1f1aec8ab1e9df82a2bc928e
Stalin <stalinbarath@gmail.com>
2022-05-30 15:43:24 +0530

    Added response status code conventions

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -3,6 +3,7 @@ from anyio import maybe_async
 from django.http import request, JsonResponse
 from watchlist_app.models import Movie
 
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from watchlist_app.api import serializers
@@ -19,28 +20,30 @@ def movies(request):
         serializer = serializers.MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','PUT','DELETE'])
 def movie(request, pk):
 
-    if request.method == 'GET':
+    try:
         movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response({'error': 'Movie not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'GET':
         serializer = serializers.MovieSerializer(movie)
         return Response(serializer.data)
 
     if request.method == 'PUT':
-        movie = Movie.objects.get(pk=pk)
         serializer = serializers.MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'DELETE':
-        movie = Movie.objects.get(pk=pk)
         movie.delete()
-        return Response(status=200)
+        return Response(status=status.HTTP_204_NO_CONTENT)
