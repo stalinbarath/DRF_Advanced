27a42b5dac8a892b7007c4b99915442c9024ed10
Stalin <stalinbarath@gmail.com>
2022-05-31 11:08:33 +0530

    Added serilizer validations of field, object and validator methods

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,15 @@
+from wsgiref.validate import validator
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+def check_movie_length(value):
+    if len(value) <= 1:
+        raise serializers.ValidationError('Movie name is too short. Include year to increase length.')
+    return value
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[check_movie_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -16,3 +22,15 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    # Field level validation
+    def validate_description(self,value):
+        if not 'lokesh' in value.lower():
+            raise serializers.ValidationError('Not a Lokesh Kanagaraj Film')
+        return value
+
+    # Object level validation
+    def validate(self,value):
+        if value['name'] == value['description']:
+            raise serializers.ValidationError('Description shouldn\'t be identical to Movie name.')
+        return value
