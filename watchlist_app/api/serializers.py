7059c2e6ef2ce9a23329d176ee662f0739192757
Stalin <stalinbarath@gmail.com>
2022-05-31 14:55:14 +0530

    Added serializer.ModelSerializer and backed-up serializers.Serializer

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,32 +1,20 @@
-from wsgiref.validate import validator
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
-def check_movie_length(value):
-    if len(value) <= 1:
-        raise serializers.ValidationError('Movie name is too short. Include year to increase length.')
-    return value
-
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[check_movie_length])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
 
     # Field level validation
     def validate_description(self,value):
         if not 'lokesh' in value.lower():
             raise serializers.ValidationError('Not a Lokesh Kanagaraj Film')
+        return value
+    
+    def validate_name(self,value):
+        if len(value) <= 1:
+            raise serializers.ValidationError('Movie name is too short. Include year to increase length.')
         return value
 
     # Object level validation
